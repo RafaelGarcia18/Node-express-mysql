eeaf1865b45c3ce311bf0b26c467970022e9c2c7
Rafael Garcia <ge17001@ues.edu.sv>
2022-03-08 15:56:07 -0600

    aving Data in Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,7 +4,14 @@ const pool = require("../database");
 router.get("/add", (req, res) => {
   res.render("links/add");
 });
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  await pool.query("INSERT INTO links set ?", [newLink]);
   res.send("received");
 });
 module.exports = router;
