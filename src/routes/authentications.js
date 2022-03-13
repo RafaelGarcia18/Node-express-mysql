03e207d394c1c3515aead57eab94d821806315fa
Rafael Garcia <ge17001@ues.edu.sv>
2022-03-12 18:40:37 -0600

    Users Signin or Login

--- src/routes/authentications.js
+++ src/routes/authentications.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport')
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("auth/signup");
+});
+
+router.get("/signin", (req, res) => {
+  res.render("auth/signin");
+});
+
+router.post("/signin", (req, res, next) => {
+  passport.authenticate("local.signin", {
+    successRedirect: "/profile",
+    failureRedirect: "/signin",
+    failureFlash: true,
+  })(req, res, next);
 });
 
 router.post(
