edc7c560ec1310d4e235f65e6711c14ed2741042
Rafael Garcia <ge17001@ues.edu.sv>
2022-04-26 14:14:51 -0600

    Protecting Routes from the Server and personaldata

--- src/routes/authentications.js
+++ src/routes/authentications.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const { isLoggedIn, isNotLoggedIn } = require("../lib/auth");
 
-router.get("/signup", (req, res) => {
+router.get("/signup", isNotLoggedIn, (req, res) => {
   res.render("auth/signup");
 });
 
-router.get("/signin", (req, res) => {
+router.get("/signin", isNotLoggedIn, (req, res) => {
   res.render("auth/signin");
 });
 
-router.post("/signin", (req, res, next) => {
+router.post("/signin", isNotLoggedIn, (req, res, next) => {
   passport.authenticate("local.signin", {
     successRedirect: "/profile",
     failureRedirect: "/signin",
@@ -20,6 +21,7 @@ router.post("/signin", (req, res, next) => {
 
 router.post(
   "/signup",
+  isNotLoggedIn,
   passport.authenticate("local.signup", {
     successRedirect: "/profile",
     failureRedirect: "/signup",
@@ -27,8 +29,13 @@ router.post(
   })
 );
 
-router.get("/profile", (req, res) => {
-  res.send("profile");
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("auth/profile");
+});
+
+router.get("/logout", isLoggedIn, (req, res) => {
+  req.logOut();
+  res.redirect("/signin");
 });
 
 module.exports = router;
